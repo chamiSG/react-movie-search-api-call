40e73deaa3684f41fb9805911d53544cbf304391
DESKTOP-TKBO95A\HADES <chamiSG>
2021-02-05 23:57:01 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,45 +1,97 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import logo from './logo.svg';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { Col, Form, Container, Table} from "react-bootstrap";
 import './App.css';
 
 
 function App() {
   const [data, setData] = useState([]);
-  const [query, setQuery] = useState("2018");
+  const [year, setYear] = useState(2018);
+  const [validated, setValidated] = useState(false);
+  const [loading, setLoading] = useState(false)
 
+
+  const handleChange = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setYear(parseInt(event.target.value) || '')
+    setValidated(true);
+  };
 
   //====== Searchbar ======
   useEffect(() => {
+    setLoading(true)
     const fetchData = () => {
       axios
-        .get("https://jsonmock.hackerrank.com/api/movies?Year=" + query)
+        .get("https://jsonmock.hackerrank.com/api/movies?Year=" + year)
           .then(({data}) => {
-            console.log(data.data);
+            setLoading(false)
             setData(data.data);
           })
           .catch(error => console.log(error));
     };
 
     fetchData();
-  }, [query]);
+  }, [year]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Container className="list-body">
+        <h2>Movie Search</h2>
+        <Form noValidate validated={validated} className="formbody">
+          <Form.Row className="justify-content-md-center">
+            <Form.Group as={Col} md="4" offset="2" controlId="validationCustom01">
+              <Form.Control type="number" placeholder="Enter the year." required value={year} onChange={handleChange} />
+            </Form.Group>
+          </Form.Row>
+        </Form>
 
-        <div className="Form">
-          <input value={query} onChange={event => setQuery(event.target.value)} />
-          <ul>
+        <Table striped bordered hover >
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Year</th>
+              <th>AD_ID</th>
+            </tr>
+          </thead>
+          <tbody>
             {data.map((element, index) => (
-              <li key={index}>
-                <p >{element.Title} / {element.Year}</p>
-              </li>
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{element.Title}</td>
+                <td>{element.Year}</td>
+                <td>{element.imdbID}</td>
+              </tr>
             ))}
-          </ul>
-        </div>
-      </header>
+
+            { loading && (
+              <tr>
+                <td colSpan="4">
+                  Loading...
+                </td>
+              </tr>
+            ) }
+
+            { !loading && data.length === 0 && (
+              <tr>
+                <td colSpan="4">
+                  No Movies
+                </td>
+              </tr>
+            ) }
+            
+          </tbody>
+        </Table>
+        
+        
+      </Container>
     </div>
   );
 }

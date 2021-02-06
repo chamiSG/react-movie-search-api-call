c6a7e61d4bc752cc44050c3c817635c0e12579d0
DESKTOP-TKBO95A\HADES <chamiSG>
2021-02-05 22:21:17 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("2018");
+
+
+  //====== Searchbar ======
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://jsonmock.hackerrank.com/api/movies?Year=" + query)
+          .then(({data}) => {
+            console.log(data.data);
+            setData(data.data);
+          })
+          .catch(error => console.log(error));
+    };
+
+    fetchData();
+  }, [query]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="Form">
+          <input value={query} onChange={event => setQuery(event.target.value)} />
+          <ul>
+            {data.map((element, index) => (
+              <li key={index}>
+                <p >{element.Title} / {element.Year}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
